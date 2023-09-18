2cdc9cbaa49a73348a2654178ef86e7014d8dede
Ivixx09 <estudio.xx23@gmail.com>
2023-09-18 08:11:40 -0300

    first commit and db connected

---
+++ db/models/index.ts
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+const config = require("../config/config.js");
+
+let sequelize;
+if (process.env.NODE_ENV === "production") {
+    sequelize = new Sequelize(config.production);
+} else {
+    sequelize = new Sequelize(config.development);
+}
+
+const connection = sequelize;
+
+module.exports = connection;
