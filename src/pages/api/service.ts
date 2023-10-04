8e24bb2a06a63976a26b7381212bafcf161231e7
Ivixx09 <estudio.xx23@gmail.com>
2023-10-04 09:58:35 -0300

    Routes for service finished

--- src/pages/api/service.ts
+++ src/pages/api/service.ts
@@ -18,7 +18,7 @@ export default async function handler(
       } else {
         const data = await Service.findAll()
         console.log('dentroo ', data)
-        if (data.legth > 0) {
+        if (data) {
           return res.status(200).json(data)
         } else {
           return res
@@ -103,6 +103,80 @@ export default async function handler(
         return res.status(400).send(e.message)
       } else {
         return res.status(400).send(e)
+      }
+    }
+  } else if (req.method === 'PUT') {
+    try {
+      const {
+        id,
+        title,
+        description,
+        price,
+        sena,
+        duration,
+        game_type,
+        grass,
+        ball,
+        length,
+        width,
+        street,
+        number,
+      } = req.body
+      if (!id) {
+        res.status(400).send('We need an id')
+      } else {
+        const service = await Service.findOne({ where: { id: id } })
+        if (!service) {
+          res.status(404).send('The service was not found')
+        } else {
+          console.log(ball)
+          await service.update({
+            title: title,
+            description: description,
+            price: price,
+            sena: sena,
+            duration: duration,
+            game_type: game_type,
+            grass: grass,
+            ball: ball,
+            length: length,
+            width: width,
+            street: street,
+            number: number,
+          })
+          return res.status(200).json(service)
+        }
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(e.message)
+      } else {
+        console.log(e)
+      }
+    }
+  } else if (req.method === 'DELETE') {
+    try {
+      const { id } = req.body
+      if (!id) {
+        return res.status(400).send('We need an id!')
+      } else {
+        const service = await Service.findOne({ where: { id: id } })
+        if (service) {
+          await service.destroy()
+          const serviceTest = await Service.findOne({ where: { id: id } })
+          if (serviceTest) {
+            return res.status(400).send('Somenthing went wrong :(')
+          } else {
+            return res.status(200).send('The service where deleted succesfully')
+          }
+        }
+        return res.status(404).send('service not found')
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message)
+      } else {
+        console.log('unexpected error', err)
       }
     }
   }
