97237d1318255d07c41ff5e53521c44eb5052624
ambrosiaman <ambrosiamanbusiness@gmail.com>
2023-10-09 17:54:20 -0300

    header component ready

---
+++ src/pages/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header(){
+    return(
+<header className="bg-blue-500 text-white text-center p-5">
+        <h1 className="text-5xl mb-5">Welcome to Cancha!</h1>
+        <p className="mb-5">Discover and book sports facilities around you</p>
+      </header>
+      )
+      }
+
+      export default Header
