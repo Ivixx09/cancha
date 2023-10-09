d2fcc65d44d01e68f0dab80874e39fc3401cd937
ambrosiaman <ambrosiamanbusiness@gmail.com>
2023-10-09 17:30:18 -0300

    cambios al header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,10 @@ export default function Home() {
       <title>Cancha - Rent Sport Courts</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
+    <header className="bg-blue-500 text-white text-center p-5">
+        <h1 className="text-5xl mb-5">Welcome to Cancha!</h1>
+        <p className="mb-5">Discover and book sports facilities around you</p>
+      </header>
     
     <main className="p-5">
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
