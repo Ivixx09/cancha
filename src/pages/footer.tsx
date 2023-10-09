37336df25720753e463695ea136668a9ffa25a0f
ambrosiaman <ambrosiamanbusiness@gmail.com>
2023-10-09 18:00:23 -0300

    Footer component ready

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -1,3 +1,9 @@
-
-
-<footer className="bg-blue-500 text-white text-center p-5"> <p>Made with love by Cancha</p> </footer>
+import React from 'react'
+function Footer (){
+    return(
+<footer className="bg-blue-500 text-white text-center p-5">
+    <p>Made with love by Cancha</p>
+    </footer>
+)
+}
+export default Footer
