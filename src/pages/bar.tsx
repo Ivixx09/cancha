4426c6917c7c0e43cd02bb677639ed900f8accbb
ambrosiaman <ambrosiamanbusiness@gmail.com>
2023-10-16 18:47:18 -0300

    the fucking select sports works

--- src/pages/bar.tsx
+++ src/pages/bar.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
 function Bar() {
+  const [sport, setSport] = useState('');
+
+  const handleSportChange = (event) => {
+    setSport(event.target.value);
+  };
     return (
         <section className="flex justify-center items-center">
             <div className="mb-10 ml-5 mr-5 bg-white rounded-xl shadow-md overflow-hidden md:inline-flex w-auto">
@@ -21,11 +25,17 @@ function Bar() {
                             </div>
                             <div className="px-3">
                                 <div className="relative">
-                                    <select className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-sport">
-                                        <option>Soccer</option>
-                                        <option>Tennis</option>
-                                        <option>Baseball</option>
-                                    </select>
+                                <select
+  value={sport || ''} // If sport is an empty string, the initial label 'Select Sport' will be shown
+  onChange={handleSportChange}
+  className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+  id="grid-sport"
+>
+  <option value='' hidden>Select Sport</option> {/* Initial label */}
+  <option value='Soccer'>Soccer</option>
+  <option value='Tennis'>Tennis</option>
+  <option value='Baseball'>Baseball</option>
+</select>
                                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 4h10l-5 7-5-7z" /></svg>
                                     </div>
@@ -50,7 +60,7 @@ function Bar() {
                             </div>
                             <div className="flex items-center px-3 py-0">
                                 <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded " type="button">
-                                    Search
+                                Search courts
                                 </button>
                             </div>
                         </div>
